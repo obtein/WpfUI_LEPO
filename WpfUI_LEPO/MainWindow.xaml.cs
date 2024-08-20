1a8fe2cc149e846f0fe8c2be990a0dd43ff257e3
tekinosmanbaran@gmail.com <tekinosmanbaran@gmail.com>
2024-08-20 17:34:48 +0300

    Add project files.

---
+++ WpfUI_LEPO/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfUI_LEPO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string [] test = { "leley", "loloy"};
+
+        public MainWindow ()
+        {
+            InitializeComponent();
+        }
+    }
+}
